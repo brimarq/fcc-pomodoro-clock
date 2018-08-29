64289941aea2baa003b510ff9d8b534e126b269e
Brian Marquis <rbmarquis.dev@gmail.com>
2018-08-28 20:09:41 -0500

    Set timers as Date objects in default state, linking them to clickHandler for break and session settings.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 const defaultClockState = {
   breakLength: 5,
   sessionLength: 25, 
+  breakTimer: new Date(Date.UTC(1970, 0, 1, 0, 5, 0)),
+  sessionTimer: new Date(Date.UTC(1970, 0, 1, 0, 25, 0)),
   isSession: true,
   isTimerRunning: false,
   countdown: null,
@@ -12,8 +14,10 @@ const defaultClockState = {
 class BreakSetting extends Component {
   render() {
     return(
-      <div id="break-setting">
-        BreakSetting
+      <div id="break-setting">BreakSetting
+        <button id="break-decrement" onClick={this.props.handleClick}>DOWN</button>
+        <span id="break-length">{this.props.breakLength}</span>
+        <button id="break-increment" onClick={this.props.handleClick}>UP</button>
       </div>
     );
   }
@@ -24,6 +28,9 @@ class SessionSetting extends Component {
     return(
       <div id="session-setting">
         SessionSetting
+        <button id="session-decrement" onClick={this.props.handleClick}>DOWN</button>
+        <span id="session-length">{this.props.sessionLength}</span>
+        <button id="session-increment" onClick={this.props.handleClick}>UP</button>
       </div>
     );
   }
@@ -46,16 +53,48 @@ class PomodoroClock extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    return;
+  handleClick(e) {
+    const eleId = e.target.id;
+    // HANDLER FOR BREAK/SESSION SETTINGS
+    if (eleId.includes('-increment') || eleId.includes('-decrement')) {
+      const amt = eleId.includes('-increment') ? 1 : -1;
+      const setting = eleId.includes('break-') ? 'break' : 'session';
+      const setTimer = (timer) => {
+        timer.setTime(timer.getTime() + (amt * 60000));
+        return timer;
+      };
+      const state = {
+        'break': (prevState) => ({ 
+          breakLength: prevState.breakLength + amt,
+          breakTimer: setTimer(prevState.breakTimer)
+        }), 
+        'session': (prevState) => ({ 
+          sessionLength: prevState.sessionLength + amt,
+          sessionTimer: setTimer(prevState.sessionTimer)
+        }) 
+      };
+      
+      this.setState(state[setting]);
+    } else {
+      return;
+    }
+  
   }
 
   render() {
     return(
       <div id="pomodoro-clock">
-        <BreakSetting />
-        <SessionSetting />
-        <Timer />
+        <BreakSetting 
+          handleClick={this.handleClick}
+          breakLength={this.state.breakLength}
+        />
+        <SessionSetting 
+          handleClick={this.handleClick}
+          sessionLength={this.state.sessionLength}
+        />
+        <Timer 
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
