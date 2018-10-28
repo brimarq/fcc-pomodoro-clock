63b8eeed91bdb8565a4ed31d76325b4cd85c80bd
Brian Marquis <rbmarquis.dev@gmail.com>
2018-10-28 18:48:44 -0500

    action-creators

---
+++ src/actions/index.js
@@ -0,0 +1,40 @@
+import { SET_BREAK_LENGTH, SET_SESSION_LENGTH, SET_IS_RUNNING, RESET_TIMER, TOGGLE_TIMER, TICK_TIMER} from "../constants/action-types";
+
+/**** ACTION CREATORS ****/
+export const setBreakLength = (length) => ({
+  type: SET_BREAK_LENGTH,
+  breakLength: length
+});
+
+export const setSessionLength = (length) => ({
+  type: SET_SESSION_LENGTH,
+  sessionLength: length
+});
+
+export const setTimer = (time) => ({
+  type: SET_TIMER,
+  timer: time
+});
+
+export const resetTimer = () => ({
+  type: RESET_TIMER
+});
+
+export const setIsBreak = (bool) => ({
+  type: SET_IS_BREAK,
+  isBreak: bool
+});
+
+export const setIsRunning = (bool) => ({
+  type: SET_IS_RUNNING,
+  isRunning: bool
+});
+
+export const toggleTimer = () => ({
+  type: TOGGLE_TIMER,
+});
+
+export const tickTimer = (time) => ({
+  type: TICK_TIMER,
+  timer: time
+});
--- src/constants/action-types.js
+++ src/constants/action-types.js
@@ -1,5 +1,6 @@
 export const SET_BREAK_LENGTH = "SET_BREAK_LENGTH";
 export const SET_SESSION_LENGTH = "SET_SESSION_LENGTH";
+export const SET_IS_RUNNING = "SET_IS_RUNNING";
 export const RESET_TIMER = "RESET_TIMER";
 export const TOGGLE_TIMER = "TOGGLE_TIMER";
 export const TICK_TIMER = "TICK_TIMER";
