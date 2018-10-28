8bcf468ed34eded2403112eb9cc7c4be99ea1915
Brian Marquis <rbmarquis.dev@gmail.com>
2018-10-28 18:33:55 -0500

    action-types

---
+++ src/constants/action-types.js
@@ -0,0 +1,5 @@
+export const SET_BREAK_LENGTH = "SET_BREAK_LENGTH";
+export const SET_SESSION_LENGTH = "SET_SESSION_LENGTH";
+export const RESET_TIMER = "RESET_TIMER";
+export const TOGGLE_TIMER = "TOGGLE_TIMER";
+export const TICK_TIMER = "TICK_TIMER";
