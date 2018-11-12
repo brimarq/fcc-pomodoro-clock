bb5ec17a4a06bf578fd1483476b9b0da267165c6
Brian Marquis <rbmarquis.dev@gmail.com>
2018-11-12 01:00:03 -0600

    chg TimerSettings to class

--- src/TimerSetting.js
+++ src/TimerSetting.js
@@ -3,7 +3,7 @@ import store from "./store";
 import "./TimerSetting.css";
 import { setBreakLength, setSessionLength } from "./actions";
 
-const TimerSetting = ({setting}) => {
+/* const TimerSetting = ({setting}) => {
   
   const { breakLength, sessionLength, isRunning } = store.getState();
   const length = setting === "break" ? breakLength : sessionLength;
@@ -26,6 +26,36 @@ const TimerSetting = ({setting}) => {
       <button id={setting + "-increment"} type="button" disabled={isRunning} onClick={handleClick}>UP</button>
     </div> 
   )
-};
+}; */
+
+class TimerSetting extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const { setting, length } = this.props;
+    const amt = e.target.id.includes('-increment') ? 1 : -1;
+    const newLength = length + amt;
+    const action = (payload) => setting === "break" ? setBreakLength(payload) : setSessionLength(payload);
+
+    if (newLength > 60 || newLength < 1 ) return;
+    return store.dispatch(action(newLength));
+  }
+
+  render() {
+    const { setting, label, length, isRunning } = this.props;
+    
+    return (
+      <div id={setting + "-setting"}>
+        <span id={setting + "-label"}>{label}</span>
+        <button id={setting + "-decrement"} type="button" disabled={isRunning} onClick={this.handleClick}>DOWN</button>
+        <span id={setting + "-length"}>{length}</span>
+        <button id={setting + "-increment"} type="button" disabled={isRunning} onClick={this.handleClick}>UP</button>
+      </div>
+    );
+  }
+}
 
 export default TimerSetting;
