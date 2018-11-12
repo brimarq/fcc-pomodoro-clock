9d7c4957e1b22321adf07043b8af5fa9ece3d263
Brian Marquis <rbmarquis.dev@gmail.com>
2018-11-12 17:20:26 -0600

    Update App with refactored components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import store from "./store";
 import TimerSetting from "./TimerSetting";
 import Timer from "./Timer";
+import TimeRing from "./TimeRing";
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">Header</header>
-      <main className="App-main">
-        <div className="pomodoro-clock">
-          <TimerSetting setting="break" />
-          <TimerSetting setting="session" />
-          <Timer />
-        </div>
-      </main>
-      <footer className="App-footer">Footer</footer>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const { isBreak, isRunning, timer, breakLength, sessionLength } = store.getState();
+    return (
+      <div className="App">
+        <header className="App-header">Header</header>
+        <main className="App-main">
+          <div className="pomodoro-clock">
+            <TimerSetting 
+              setting="break" 
+              label="Break Length" 
+              length={breakLength}  
+              isRunning={isRunning}  
+            />
+            <TimerSetting 
+              setting="session" 
+              label="Session Length" 
+              length={sessionLength}  
+              isRunning={isRunning} 
+            />
+            <Timer 
+              isBreak={isBreak}
+              isRunning={isRunning}
+              timer={timer}
+              breakLength={breakLength}
+              sessionLength={sessionLength}
+            />
+            <TimeRing {...store.getState()} />
+          </div>
+        </main>
+        <footer className="App-footer">Footer</footer>
+      </div>
+    );
+  }
 }
-
 export default App;
