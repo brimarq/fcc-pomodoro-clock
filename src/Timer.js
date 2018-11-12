5bf267daced495787b49aaa139cab6078f028d8f
Brian Marquis <rbmarquis.dev@gmail.com>
2018-11-12 17:18:57 -0600

    Refactor Timer as PureComponent, remove TimeRing

--- src/Timer.js
+++ src/Timer.js
@@ -1,72 +1,79 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import store from "./store";
 import "./Timer.css";
-import TimeRing from "./TimeRing";
+// import anime from 'animejs'
+// import TimeRing from "./TimeRing";
 import TimeLeft from "./TimeLeft";
 import { tickTimer, toggleTimer, setIsRunning, resetTimer } from "./actions";
 
-const Timer = () => {
-  const { isBreak, isRunning, timer, breakLength, sessionLength } = store.getState();
-  
-  const timerLabel = isBreak ? "Break" : "Session";
-  const btnToggleLabel = isRunning ? "STOP" : "START";
-  let audioElement = React.createRef();
 
-  const handleTimer = () => {
-    const beep = audioElement.current;
+class Timer extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.audioElement = React.createRef();
+    this.handleTimer = this.handleTimer.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleTimer() {
+    const { isRunning } = this.props;
+    const beep = this.audioElement.current;
+    
     const startTimer = () => {
-      
-      const countdown = () => {
-        const time = store.getState().timer;
+
+      const countdown = (time = store.getState().timer) => {
         const newTime = time - 1;
         if (!time) {
-          beep.play();
+          //beep.play();
           store.dispatch(toggleTimer());
         } else {
           store.dispatch(tickTimer(newTime));
-          
+          if (!newTime) beep.play();
         }
-        
-        if (!newTime) beep.play();
       };
 
-      window.timerID = setInterval(countdown, 1000);
+      this.timerID = setInterval(countdown, 1000);
     };
 
-    isRunning ? clearInterval(window.timerID) : startTimer();
-  };
+    isRunning ? clearInterval(this.timerID) : startTimer();
+  }
 
-  const handleClick = (e) => {
+  handleClick(e) {
     const clickedId = e.target.id;
-    const beep = audioElement.current;
+    const beep = this.audioElement.current;
     const toggle = (bool) => {
       store.dispatch(setIsRunning(bool));
-      handleTimer();
-
+      this.handleTimer();
     };
     const reset = () => {
-      if (isRunning) clearInterval(window.timerID);
+      if (this.props.isRunning) clearInterval(this.timerID);
       beep.pause();
       beep.currentTime = 0;
       store.dispatch(resetTimer());
-    };
-    clickedId === "reset" ? reset() : toggle(!isRunning);
-  };
-
-  
-  
-  return (
-    <div id="timer">
-      <TimeRing remTime={timer} totalTime={isBreak ? breakLength * 60 : sessionLength * 60} />
       
-      <div id="timer-label">{timerLabel}</div>
-      <TimeLeft />
-      <button id="start_stop" type="button" onClick={handleClick}>{btnToggleLabel}</button>
-      <button id="reset" type="button" onClick={handleClick}>RESET</button>
-      <audio id="beep" src="https://freesound.org/data/previews/250/250629_4486188-lq.mp3" type="audio/mpeg" ref={audioElement}></audio>
-    </div>
-  );
-};
+    };
+    clickedId === "reset" ? reset() : toggle(!this.props.isRunning);
+  }
+
+
+  render() {
+    const { isBreak, isRunning } = this.props;
+    const timerLabel = isBreak ? "Break" : "Session";
+    const btnToggleLabel = isRunning ? "STOP" : "START";
+    
+    return (
+      <div id="timer">
+        
+        <div id="timer-label">{timerLabel}</div>
+        <TimeLeft />
+        <button id="start_stop" type="button" onClick={this.handleClick}>{btnToggleLabel}</button>
+        <button id="reset" type="button" onClick={this.handleClick}>RESET</button>
+        <audio id="beep" src="https://freesound.org/data/previews/250/250629_4486188-lq.mp3" type="audio/mpeg" ref={this.audioElement}></audio>
+        
+      </div>
+    );
+  }
+}
+
 
 export default Timer;
