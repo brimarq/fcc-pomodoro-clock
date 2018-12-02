82bdcf74b9891222a9a9a6c4fc9249c84e2a230a
Brian Marquis <rbmarquis.dev@gmail.com>
2018-12-01 19:04:13 -0600

    Add hover effect, text coloring to buttons

--- src/TimerControls.js
+++ src/TimerControls.js
@@ -7,7 +7,7 @@ class TimerControls extends PureComponent {
     const startStopLabel = isRunning ? "PAUSE" : "START";
     return (
       <div id="timer-controls">
-       <button id="start_stop" className="btn-controls" type="button" onClick={handleTimerControl}>{startStopLabel}</button>
+       <button id="start_stop" className="btn-controls" type="button" onClick={handleTimerControl} style={ isRunning ? {"--rubber-pad-color": "hsla(60, 100%, 75%, 1)"} : {"--rubber-pad-color": "hsla(120, 100%, 75%, 1)"}}>{startStopLabel}</button>
        <button id="reset" className="btn-controls" type="button" onClick={handleTimerControl}>RESET</button>
      </div>
     );
